62d90436f5c3d4a36c70e757b1f7f07b76ead2af
main <aiellof@bzz.ch>
2024-09-02 14:22:36 +0200

    Refinement

--- main.py
+++ main.py
@@ -3,35 +3,43 @@ Created on Fri Mar 12 14:
 """
 import json
 
+
 # Funktion zum Laden eines Rezepts aus einem JSON-String
-def load_recipe(json_string):
-    return json.loads(json_string)
+def load_recipe(json_str):
+    return json.loads(json_str)
+
 
 # Funktion zur Anpassung der Rezeptmengen für eine variable Personenanzahl
-def adjust_recipe(recipe, new_servings):
-    factor = new_servings / recipe['servings']
-    adjusted_ingredients = {ingredient: amount * factor for ingredient, amount in recipe['ingredients'].items()}
+def adjust_recipe(original_recipe, servings):
+    factor = servings / original_recipe['servings']
+    adjusted_ingredients = {
+        ingredient: amount * factor
+        for ingredient, amount in original_recipe['ingredients'].items()
+    }
     # Neues angepasstes Rezept zurückgeben, ohne das Original zu verändern
     return {
-        'title': recipe['title'],
+        'title': original_recipe['title'],
         'ingredients': adjusted_ingredients,
-        'servings': new_servings
+        'servings': servings
     }
 
 
 if __name__ == '__main__':
     # Beispiel für die Datenstruktur eines Rezepts als JSON-String
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    recipe_json = (
+        '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, '
+        '"Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    )
 
     # Rezept aus JSON laden
-    recipe = load_recipe(recipe_json)
+    original_recipe = load_recipe(recipe_json)
 
     # Neue Anzahl an Personen
-    new_servings = 2
+    new_serving_size = 2
 
     # Rezept für neue Anzahl an Personen anpassen
-    adjusted_recipe = adjust_recipe(recipe, new_servings)
+    adjusted_recipe = adjust_recipe(original_recipe, new_serving_size)
 
     # Angepasstes Rezept anzeigen
-    print("Originales Rezept:", recipe)
-    print("\nAngepasstes Rezept:", adjusted_recipe)
+    print('Originales Rezept:', original_recipe)
+    print('\nAngepasstes Rezept:', adjusted_recipe)
